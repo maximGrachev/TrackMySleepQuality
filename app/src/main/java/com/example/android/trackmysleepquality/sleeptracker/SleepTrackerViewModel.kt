763e788fbd22a229919d4216ace8cd65aad3da41
Maxim Grachev <mxmgrachev@gmail.com>
2020-09-08 20:51:32 +0300

    6.15 Starting coroutines functions

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -3,6 +3,7 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import kotlinx.coroutines.Job
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -10,5 +11,8 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
+
+    private var viewModelJob = Job()
+
 }
 
